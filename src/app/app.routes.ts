e663046c3f393efba2f24b37890fd3789597d2a2
jasjas24 <banaszkm@gmail.com>
2026-07-13 10:44:13 +0200

    260713_1044

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Login } from './login/login';
 import { Profil } from './profil/profil';
+import { Kanban } from './kanban/kanban';
+import { Dashboard } from './dashboard/dashboard';
 
 export const routes: Routes = [
     { path: '', component: Home, title: 'MOVE:botics' },
     { path: 'login', component: Login, title: 'Intranet' },
-    { path: 'profil', component: Profil, title: 'Profil' }
+    { path: 'profil', component: Profil, title: 'Profil' },
+    { path: 'kanban', component: Profil, title: 'Kanban' },
+    { path: 'dashboard', component: Profil, title: 'Dashboard' }
 ];
